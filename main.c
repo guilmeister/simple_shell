3e84a4de547e4ab421d5bf9d54b8f51667392a37
Trevor-Ofarrell <744@holbertonschool.com>
2019-08-27 07:45:20 +0000

    orginizing

--- main.c
+++ main.c
@@ -6,11 +6,9 @@ void signal_handler(int signum)
 	_putchar('\n');
 	write(STDOUT_FILENO, "$ ", 2);
 }
-
 int main(int argc, char *argv[], char **env)
 {
-	char *buffer = NULL;
-	char **token = NULL;
+	char *buffer = NULL, **token = NULL;
 	size_t length = 0;
 	int check, counter = 0;
 	pid_t parentpid = getpid();
@@ -49,35 +47,4 @@ int main(int argc, char *argv[], char **env)
 		free(token);	}
 	decrementBuffer(buffer, counter);
 	return (EXIT_SUCCESS);
-}
-
-int my_cd(char **args)
-{
-	if (args[1] == NULL)
-	{
-		perror("Error");
-	}
-	else if (chdir(args[1]) != 0)
-	{
-		perror("Error");
-	}
-	return (EXIT_SUCCESS);
-}
-
-int my_help(char **args)
-{
-	args++;
-	write(2, "Welcome to Trevor's and Ed's simple Unix shell!\n", 56);
-	write(2, "type program names and arguments and press enter\n", 56);
-	write(2, "cd, help, env, and exit are built in commmands\n", 48);
-	write(2, "Use the man command to find info on other programs\n", 52);
-	return (EXIT_SUCCESS);
-}
-
-int my_exit(char **args)
-{
-	while (1 || args)
-	{
-		exit(EXIT_SUCCESS);
-	}
 }
